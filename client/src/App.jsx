580333324da37c1c7f9bab7b4846ba950f0cac42
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-07-26 21:31:36 +0100

    set up Nav Bar &&  router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 
 
+import { NavLink, Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
   
   return (
     <>
      <div className='w-full min-h-svh bg-custom-gradient  '>
-
+        <Navbar/>
+        <Outlet/>
      </div>
     </>
   )
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar (){
+
+
+    return (
+        <>
+        <header className="w-full min-h-24 bg-zinc-800/80 border-b-2 border-zinc-200 flex justify-between items-center px-10">
+            <div>
+                <h1 className=" text-4xl font-semibold text-zinc-200 ">Inventory</h1>
+            </div>
+            <nav className="text-xl font-semibold text-zinc-200 w-fit">
+                
+                <NavLink to="/categorie" className=" hover:animate-pulse w-6 h-6 mx-4"> item </NavLink>
+                    <NavLink to="/categorie" className=" hover:animate-pulse w-6 h-6"> categorie</NavLink>
+            </nav>
+        </header>
+        </>
+    )
+}
+export default Navbar
