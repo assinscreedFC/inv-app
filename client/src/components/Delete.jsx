2eccede29cbcfa2c9106536534538629db9a72b9
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-08-02 03:45:08 +0100

    gege

--- client/src/components/Delete.jsx
+++ client/src/components/Delete.jsx
@@ -40,7 +40,7 @@ function Delete() {
                 <div className="w-[45%] md:w-[31%] bg-zinc-200 rounded-md p-2 flex flex-col gap-1 m-2">
                     <form className="w-full my-2" onSubmit={sub}>
                         <div className="w-full flex justify-center gap-2 items-center">
-                            <label htmlFor="password" className="font-semibold text-xl">Password:</label>
+                            <label htmlFor="password" className="font-semibold text-lg lg:text-xl ">Password:</label>
                             <input
                                 type="password"
                                 name="password"
@@ -54,8 +54,8 @@ function Delete() {
                             />
                         </div>
                         <div className="w-full items-center justify-between flex mt-4 px-2">
-                        <button type="button"  className="px-4 py-1 rounded-md ring-green-600 ring-4 text-slate-50 flex justify-center items-center font-semibold text-xl bg-slate-800 my-2" onClick={()=>navigate("../items")}>Return</button>
-                            <button type="submit" className="px-4 py-1 rounded-md ring-green-600 ring-4 text-slate-50 flex justify-center items-center font-semibold text-xl bg-slate-800 my-2">Delete</button>
+                        <button type="button"  className="w-[40%] px-4 py-1 rounded-md ring-green-600 ring-4 text-slate-50 flex justify-center items-center font-semibold text-lg lg:text-xl  bg-slate-800 my-2" onClick={()=>navigate("../items")}>Return</button>
+                            <button type="submit" className=" w-[40%] px-4 py-1 rounded-md ring-green-600 ring-4 text-slate-50 flex justify-center items-center font-semibold text-lg lg:text-xl  bg-slate-800 my-2">Delete</button>
                         </div>
                     </form>
                 </div>
