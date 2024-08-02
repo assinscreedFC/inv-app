fa8f80d82a990b5fbfe3f88d7de2d75d1db9394e
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-08-02 03:41:24 +0100

    csc

--- client/src/pages/Items.jsx
+++ client/src/pages/Items.jsx
@@ -31,7 +31,7 @@ function Items() {
   }, [bdd, name]);
 
   return (
-    <div className="flex flex-col lg:w-full items-center gap-4">
+    <div className="flex flex-col w-full items-center gap-4">
       <NavLink to="new" className="px-4 w-fit py-1 rounded-md ring-green-600 ring-4 text-slate-50 flex justify-center items-center font-semibold text-xl bg-slate-800">NEW</NavLink>
       <div className="w-full flex flex-wrap gap-4 justify-center">
         {rr}
